b7b20165cb0d0c1ea0c5c2053f8b10c95bdc5cf9
trevor-shepard <trevorbshepard@gmail.com>
2018-12-12 14:53:53 -0800

    Complete login container/component

---
+++ frontend/components/session_form/session_container.jsx
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+
+import SessionForm from './session_form';
+
+const mapStateToProps = (state) => ({
+  errors: state.errors.session
+})
+
+const mapDispatchToProps = dispatch => ({
+  login: (user) => dispatch(login(user)),
+
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
+
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+
+class SessionForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state)
+        this.props.login(user);
+    }
+
+    handleInput(field) {
+        return (e) => this.setState({[field]: e.target.value})
+    }
+
+    render() {
+        let error
+        if (this.props.errors) {
+                                                                                                // TODO: add warn image
+            error = <span className='alert card'>"Login unsuccessful"</span>
+        }
+
+        return (
+            <div>
+                { error }
+                <form onSubmit={this.handleSubmit}>
+                    <input type="hidden" name=""/>
+                    <p>Select the area where you receive care</p>
+                    <select>
+                        <option>California, Oregon and SW Washington</option>
+                        <option>Alaska</option>
+                        <option>California</option>
+                        <option>Montana</option>
+                        <option>Northeast Oregon - Wallowa</option>
+                        <option>Oregon</option>
+                        <option>SW Washington (Clark & Cowlitz Counties)</option>
+                        <option>Washington (excluding Clark & Cowlitz Counties)</option>
+                    </select>
+                    <label>
+                        MyChart Username
+                        <input type="text" onChange={this.handleInput('username')} />
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" onChange={this.handleInput('password')} />
+                    </label>
+                    <button>SIGN IN</button>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default SessionForm;
